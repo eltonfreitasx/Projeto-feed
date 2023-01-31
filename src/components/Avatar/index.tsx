989de368f504ec25a3a9e458093b75bbdb64b1ee
Elton Freitas <xelton207@gmail>
2023-01-31 03:40:54 -0300

    concluido !

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,18 +1,16 @@
 import styles from "./Avatar.module.css";
+import { ImgHTMLAttributes } from "react";
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  src: string;
-  alt?: string;
 }
 
-export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
 
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...props}
     />
   );
 }
